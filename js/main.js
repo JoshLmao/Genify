ee3a97ad9ffa5f388eeb3f206614a9fb48e86193
Josh Shepherd <josh_lmao@live.co.uk>
2019-02-09 18:42:41 +0000

    Changed from GET to redirecu auth

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 $(() => {
     const getSpotify = () => {
-        spotify.getCredentials();
+        spotify.redirectUser();
     } 
 
     $("#btnSignIn").click(() => {
         console.log("Sign into Spotify");
         getSpotify();
     });
+
+    const readHash = () => {
+        if (location.hash && location.hash !== "#") 
+        {
+            const raw_data = location.hash.substring(1);
+            console.log(raw_data);   
+            
+            debugger;
+            const data = raw_data.split(",");
+            const decoded = decodeURI(data[0]);
+        }
+    }
+
+    readHash();
 });
--- js/spotify.js
+++ js/spotify.js
@@ -1,47 +1,11 @@
 var spotify = function(){}
 
-spotify.getCredentials = function () {
+spotify.redirectUser = function () {
     var clientId = "f4dc97c399124fc99254c5d7ac2bf4bd";
-    var respType = "code";
-    var redirectUri = "http://localhost:8000";
-    var url = `https://accounts.spotify.com/authorize?client_id=${clientId}$response_type=${respType}%redirect_uri=${redirectUri}`;
-    
-    $.getJSON(`https://curl.io/?${url}`, function (response) {
-        console.log(`resp is ${response}`);
-    });
-
-    // hitApi(`https://curl.io/?${url}`, function(error, json) {
-    //     if (error) 
-    //     {
-    //         console.log('there was an error', error);
-    //     }
-    //     else 
-    //     {
-    //         console.log('data is', data);
-    //     }
-    // });
+    var respType = "token";
+    var redirectUri = encodeURIComponent("https://genify.joshlmao.com");
+    var scopes = encodeURIComponent('streaming');
+    var apiUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=${respType}&redirect_uri=${redirectUri}&scope=${scopes}`;
+ 
+    window.location.href = apiUrl;
 }
-
-function hitApi(url, callback) {
-    var req = new XMLHttpRequest();
-  
-    req.addEventListener('load', onLoad);
-    req.addEventListener('error', onFail);
-    req.addEventListener('abort', onFail);
-  
-    req.open('GET', url);
-    req.send();
-  
-    function onLoad(event) {
-      if (req.status >= 400) {
-        onFail(event);
-      } else {
-        var json = JSON.parse(this.responseText);
-        callback(null, json);
-      }
-    }
-  
-    function onFail(event) {
-      callback(new Error('...'));
-    }
-  }
