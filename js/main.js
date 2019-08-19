6ee46aa414ab8fd481b01d7ffa6152021b3bfb31
Josh Shepherd <josh_lmao@live.co.uk>
2019-08-19 14:51:59 +0100

    Added sidebar for settings

--- js/main.js
+++ js/main.js
@@ -247,3 +247,13 @@ $(() => {
     initPage();
     readHash();
 });
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
